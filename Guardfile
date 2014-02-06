c875358fc942764a280af988cfc19b1aca97e2d8
curi <curi1119@gmail.com>
2014-02-06 14:43:29 +0900

    fix options to fix new guard

--- Guardfile
+++ Guardfile
@@ -1,7 +1,7 @@
 # A sample Guardfile
 # More info at https://github.com/guard/guard#readme
-
 guard :rspec, cli: "--color --format nested" do
+guard :rspec, cmd: 'rspec --color --format nested' do
   watch(%r{^spec/.+_spec\.rb$})
   #watch(%r{^spec/support/(.+)\.rb$'}) { |m| p m }
   watch('spec/support/models.rb') { "spec" }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,7 @@ require 'factories/models'
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
-  config.filter_run :focus
+  # config.filter_run :focus
 
   config.include FactoryGirl::Syntax::Methods
   # Run specs in random order to surface order dependencies. If you find an
